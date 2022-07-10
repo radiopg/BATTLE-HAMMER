3da8ee11a801973bd920ba0f26caada36c8bffc3
radiopg <107581788+radiopg@users.noreply.github.com>
2022-07-10 17:23:25 -0400

    init commit
    continuing battle hammer under new title

---
+++ Assets/Scripts/OverAllScripts/DataPersistScript.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DataPersistScript : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public static DataPersistScript Instance;
+
+    private void Awake()
+    {
+        if (Instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        Instance = this;
+        DontDestroyOnLoad(gameObject);
+    }
+}
