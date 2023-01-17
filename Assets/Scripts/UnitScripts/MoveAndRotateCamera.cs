77129724b1446069be03126b51df057657c86513
radiopg <107581788+radiopg@users.noreply.github.com>
2023-01-17 14:55:44 -0500

    worked on camera positioning
    at this point the camera is located 3 units about the controlled character - still trying to figure out how to accurately relocate the camera so it is 3 units away from a point above the controlled character

--- Assets/Scripts/UnitScripts/MoveAndRotateCamera.cs
+++ Assets/Scripts/UnitScripts/MoveAndRotateCamera.cs
@@ -7,32 +7,42 @@ public class MoveAndRotateCamera : MonoBehaviour
     public Vector2 turn;
 
     public GameObject allyGroup1;
+
+    public Vector3 cameraOffset;
     
 
     // Start is called before the first frame update
     void Start()
     {
         allyGroup1 = GameObject.Find("AllyGroup1");
+        cameraOffset = transform.position - allyGroup1.transform.position;
     }
 
     // Update is called once per frame
     void LateUpdate()
     {
-        MoveCamera();
-        //RotateCamera();
+        
+        RotateCamera();
     }
 
     void MoveCamera()
     {
-
+        transform.position = new Vector3(allyGroup1.transform.position.x, 
+                                         allyGroup1.transform.position.y + 3.0f,
+                                         allyGroup1.transform.position.z);
+        
+        
     }
 
     void RotateCamera()
     {
         turn.x += Input.GetAxis("Mouse X");
         turn.y += Input.GetAxis("Mouse Y");
-        //transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
-        //transform.RotateAround(allyGroup1.transform.position, new Vector3(turn.x, turn.y, 0), 1.0f*Time.deltaTime);
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+
+        MoveCamera();
+        
+        
         
     }
 }
