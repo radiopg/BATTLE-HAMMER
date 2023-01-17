ebc19a8313ba48ef5cb87661b47c849a8a351a1e
radiopg <107581788+radiopg@users.noreply.github.com>
2023-01-17 16:28:14 -0500

    getting closer with camera position
    this is a closer step to the camera positioning that i need

---
+++ Assets/Scripts/UnitScripts/RotateCamera.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RotateCamera : MonoBehaviour
+{
+
+    public Vector2 turn;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        turn.x += Input.GetAxis("Mouse X");
+        turn.y += Input.GetAxis("Mouse Y");
+        transform.localRotation = Quaternion.Euler(-turn.y, turn.x, 0);
+    }
+}
